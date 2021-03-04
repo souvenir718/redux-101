2c459fad8f9056cfcfd689c70e88ea8317e65c36
souvenir718 <mongkey0@naver.com>
2021-03-05 08:36:21 +0900

    createReducer

--- src/store.js
+++ src/store.js
@@ -1,20 +1,28 @@
 import { createStore } from 'redux';
-import { createAction } from '@reduxjs/toolkit';
+import { createAction, createReducer } from '@reduxjs/toolkit';
 
 const addToDo = createAction('ADD');
 const deleteToDo = createAction('DELETE');
 
-const reducer = (state = [], action) => {
-    switch (action.type) {
-        case addToDo.type:
-            console.log(action);
-            return [{ text: action.payload, id: Date.now() }, ...state];
-        case deleteToDo.type:
-            return state.filter((toDo) => toDo.id !== action.payload);
-        default:
-            return state;
-    }
-};
+// const reducer = (state = [], action) => {
+//     switch (action.type) {
+//         case addToDo.type:
+//             console.log(action);
+//             return [{ text: action.payload, id: Date.now() }, ...state];
+//         case deleteToDo.type:
+//             return state.filter((toDo) => toDo.id !== action.payload);
+//         default:
+//             return state;
+//     }
+// };
+
+// 리턴할때는 새로운 state여야 한다.
+const reducer = createReducer([], {
+    [addToDo]: (state, action) => {
+        state.push({ text: action.payload, id: Date.now() });
+    },
+    [deleteToDo]: (state, action) => state.filter((toDo) => toDo.id !== action.payload),
+});
 
 // const ADD = 'ADD';
 // const DELETE = 'DELETE';
